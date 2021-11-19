d8dba29ddab249a2c08bcba8ab2515689fafb880
Subhajit Sahu <wolfram77@gmail.com>
2021-11-19 08:30:54 +0530

    :skunk: w/ out (temporal)

--- main.cxx
+++ main.cxx
@@ -9,15 +9,6 @@
 using namespace std;
 
 
-// void runPagerank(const string& data, int repeat) {
-//   int M = countLines(data), steps = 10;
-//   printf("Temporal edges: %d\n", M);
-//   for (int batch=10, i=0; batch<M; batch*=i&1? 2:5, i++) {
-//     int skip = max(M/steps - batch, 0);
-//     printf("\n# Batch size %.0e\n", (double) batch);
-//     runPagerankBatch(data, repeat, skip, batch);
-//   }
-// }
 
 
 // SHOW
